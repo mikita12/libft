8921b5fe4382b8e3456ed0ec184bb9407bec8259
mikita12 <patronikm@gmail.com>
2025-11-11 00:17:35 +0000

    good

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,11 @@
 void *ft_memcpy(void *dest, const void *src, size_t n){
     const unsigned char * content = src;
     unsigned char * dContent = dest; 
-    while(n && *content){
+    while(n--q){
         *dContent = *content;
         content++;
         dContent++;
-        n--;
     }
+    
+    return *dest;
 } 
